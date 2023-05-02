647aea9794ec78c6653ca291a5f7fd5eea45701d
fadzniaidil <aidilfadzni@gmail.com>
2023-05-02 15:41:14 +0800

    1

---
+++ 1/zombiefeeding.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+/**
+   * @title test
+   * @dev test
+   * @custom:dev-run-script zombiefeeding.sol
+   */
+pragma solidity =0.8.18;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
+  }
+
+}
